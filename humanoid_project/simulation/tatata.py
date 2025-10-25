6ecb512073593bda5d7a010c290f6f5ddb7fdd84
Harshith Gangaraju <harshith.transact@gmail.com>
2025-10-26 00:16:31 +0530

    .

---
+++ humanoid_project/simulation/tatata.py
@@ -0,0 +1,6 @@
+import pybullet as p, pybullet_data
+
+p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.loadURDF("task2/humanoid.urdf", [0, 0, 1])
+input("Press Enter to exit...")
